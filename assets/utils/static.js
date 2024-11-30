857f4e16b514a9c0f9d6eea6eb117799cc9dc1b5
Jojo <jojocraftde@proton.me>
2024-11-30 13:45:42 +0100

    uploaded source unencrypted

---
+++ assets/utils/static.js
@@ -0,0 +1,11 @@
+module.exports.defaultPFP = 'https://cdn.discordapp.com/embed/avatars/0.png';
+
+module.exports.DummyUser = {
+    bot: false,
+    id: '991240644867674254',
+    tag: "#0000",
+    name: "Slaylish",
+    username: "skyepaw",
+    hexAccentColor: "#FFFFFF",
+    avatarURL: () => 'https://cdn.discordapp.com/embed/avatars/0.png'
+}
